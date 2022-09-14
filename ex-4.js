6eaae342fc61c5ab6bcc8b26174d26f51f9ca827
Paul Ren <pause.forever@gmail.com>
2022-09-14 15:16:14 +0700

    add ex files

---
+++ ex-4.js
@@ -0,0 +1,9 @@
+let getJohnProfile = () => {
+  return new Promise(function (resolve) {
+    setTimeout(() => resolve({
+      name: "John",
+      age: 20,
+      hobbies: ["Coding", "Football"]
+		}), 1000);
+  });
+};
---
+++ ex-5.js
@@ -0,0 +1,8 @@
+let getJohnProfile = () => {
+  return new Promise(function (_, reject) {
+    setTimeout(() => reject({
+      errorCode: 500,
+      message: "👿 Failed to request data from server",
+		}), 2000);
+  });
+};
